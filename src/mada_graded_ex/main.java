e90f6374b5e86796c5bf1175b44febed8468795e
Samuel Keller <samuel.keller@students.fhnw.ch>
2019-04-22 13:47:25 +0200

    init

---
+++ src/mada_graded_ex/main.java
@@ -0,0 +1,43 @@
+package mada_graded_ex;
+
+import java.math.*;
+import java.util.*;
+public class main {
+
+	public static void main(String[] args) {
+	 
+	    int bitLength = 100;
+		BigInteger p = BigInteger.probablePrime(bitLength, new Random());
+		BigInteger q = BigInteger.probablePrime(bitLength, new Random());
+		BigInteger n;
+		
+		if (!p.equals(q) && p.isProbablePrime(bitLength) && q.isProbablePrime(bitLength)) {
+			n = p.multiply(q);
+			System.out.println("p: " + p + "; q: " + q + "; n: " +n);
+			
+			
+			// TODO Create Eulers totient function
+			// TODO Create e from phi(n)
+			// TODO Create d
+			// TODO Implement extended eclidian algorythm
+			
+		}
+		else {
+			
+			System.out.println("Fail, same primes or p / q not a prime");
+			System.out.println("p: " + p + "; q: " + q);
+		}
+	
+	}
+	
+	/**
+	 * phi(n)
+	 * 
+	 * @param n, prime
+	 * @return n-1
+	 */
+	public BigInteger phi(BigInteger n) {
+			return n.subtract(BigInteger.ONE);  
+	}
+
+}
